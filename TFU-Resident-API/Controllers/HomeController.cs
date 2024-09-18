a73726b1816d2beb25b9d6feec77a306d9cdf9b7
tuanddhe-151088 <tuanddhe151088@fpt.edu.vn>
2024-09-18 15:58:39 +0700

    Test

---
+++ TFU-Resident-API/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TFU_Resident_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello Sep490");
+        }
+    }
+}
